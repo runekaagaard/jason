53d2bf2c8ea9b04cf5cc4c59eee77e70e5ca2087
Rune Kaagaard <rumi.kg@gmail.com>
2014-06-24 17:36:54 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='jason',
-    version='0.1.0',
+    version='0.1.1',
     author=u'Rune Kaagaard',
     author_email='rumi.kg@gmail.com',
     packages=find_packages(),
