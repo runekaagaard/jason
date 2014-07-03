92d5945ba058ee6476a99513606b25e5f3bb70d1
Rune Kaagaard <rumi.kg@gmail.com>
2014-07-03 22:02:58 +0200

    100% coverage. Adding travis conf

--- tests/test_jason.py
+++ tests/test_jason.py
@@ -24,10 +24,104 @@ def test_json_response():
     assert response['Content-Type'] == 'application/json'
 
 @jason.view(allowed_methods=['GET'])
-def x(request):
+def view1(request):
     return {'cool': 42},
 
-def test_view():
-    assert json.loads(x(HttpRequest()).content.decode("utf-8")) == {
-        "status": 405, "message": "Method Not Allowed", "data": {}
+def test_view_method_not_allowed():
+    request = HttpRequest()
+    request.method = 'POST'
+    assert json.loads(view1(request).content.decode("utf-8")) == {
+        "status": 405, "message": "Method Not Allowed", "data": {},
+    }
+
+@jason.view(allowed_methods=['GET'])
+def view2(request):
+    return {'cool': 42},
+
+def test_view_ok():
+    request = HttpRequest()
+    request.method = 'GET'
+    assert json.loads(view2(request).content.decode("utf-8")) == {
+        "status": 200, "message": "OK", "data": {'cool': 42},
+    }
+
+@jason.view(allowed_methods=['GET'])
+def view3(request):
+    raise jason.Bail({}, 402, "Nooooo")
+
+def test_view_bail():
+    request = HttpRequest()
+    request.method = 'GET'
+    assert json.loads(view3(request).content.decode("utf-8")) == {
+        "status": 402, "message": "Nooooo", "data": {},
+    }
+
+class CustomException(Exception):
+    pass
+
+@jason.view(allowed_methods=['GET'])
+def view4(request):
+    raise CustomException()
+
+def test_view_other_exception():
+    request = HttpRequest()
+    request.method = 'GET'
+    assert json.loads(view4(request).content.decode("utf-8")) == {
+        "status": 500, "message": "Internal Server Error", "data": {},
+    }
+
+@jason.view(allowed_methods=['GET'], exceptions={
+    CustomException: lambda e: (19, 278, 'Custom'),
+})
+def view5(request):
+    raise CustomException()
+
+def test_view_custom_exception():
+    request = HttpRequest()
+    request.method = 'GET'
+    assert json.loads(view5(request).content.decode("utf-8")) == {
+        "status": 278, "message": "Custom", "data": 19,
+    }
+
+@jason.view(allowed_methods=['GET'], exceptions={
+    CustomException: lambda e: (19, 278, 'Custom'),
+})
+def view6(request):
+    raise Exception()
+
+def test_view_base_exception():
+    request = HttpRequest()
+    request.method = 'GET'
+    assert json.loads(view6(request).content.decode("utf-8")) == {
+        "status": 500, "message": "Internal Server Error", "data": {},
+    }
+
+class UserHasPerm():
+    def has_perm(self, perm):
+        return perm == 'myapp.perm_ok'
+
+@jason.permission_required('myapp.perm_ok')
+@jason.view(allowed_methods=['GET'])
+def view7(request):
+    return {'cool': 42},
+
+def test_permission_required_has_perm():
+    request = HttpRequest()
+    request.method = 'GET'
+    request.user = UserHasPerm()
+    assert json.loads(view7(request).content.decode("utf-8")) == {
+        "status": 200, "message": "OK", "data": {'cool': 42},
+    }
+
+@jason.permission_required('myapp.perm_not_ok')
+@jason.view(allowed_methods=['GET'])
+def view8(request):
+    return {'cool': 42},
+
+def test_permission_required_hasnt_perm():
+    request = HttpRequest()
+    request.method = 'GET'
+    request.user = UserHasPerm()
+    assert json.loads(view8(request).content.decode("utf-8")) == {
+        "status": 401, "message": "Unauthorized", "data": {},
     }
